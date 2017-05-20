685b505c71a7959157201d16b5c6ea80d0974a01
Taisuke Yasuda <taisukey@andrew.cmu.edu>
2017-05-20 17:00:43 -0400

    add scatter plot of first spike

--- scripts/scatter-first-col.R
+++ scripts/scatter-first-col.R
@@ -1,6 +1,7 @@
 # Taisuke Yasuda
 #
-# Plots and saves the histogram of the first trials of the epsp data.
+# Plots and saves the scatter plot of the first trials of the epsp data. This is
+# used to check the stationarity of our experiment across trials.
 
 setwd("~/Dropbox/carnegie_mellon/research/neuro-summer-2017/")
 
@@ -17,11 +18,13 @@ for (i in 1:length(file.names)) {
   fileroot <- FileRoot(filename)
   file <- read.csv(file=paste("data/epsp-data/",filename,sep=""),
                    header=TRUE, sep=",")
-  pdf(paste("plots/histogram-first-col/",fileroot,".pdf", sep=""))
-  hist(unlist(file[2]),
-       main=paste("Histogram of First Spike of",fileroot),
-       xlab="Amplitude (mV)",
-       ylab="Frequency")
+  pdf(paste("plots/scatter-first-col/",fileroot,".pdf", sep=""))
+  plot(X2 ~ X,
+       data=file,
+       main=paste("Scatter Plot of First Spike of",fileroot),
+       xlab="Trial",
+       ylab="Amplitude")
+  abline(lm(X2 ~ X, data=file), col="blue")
   dev.off()
 }
 
